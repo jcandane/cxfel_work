756dd7436206beb6d9a530440babd67d5b64a368
Julio Candanedo <55640844+jcandane@users.noreply.github.com>
2024-07-22 12:25:04 -0500

    Add files via upload

--- jdist.py
+++ jdist.py
@@ -1,18 +1,25 @@
-from functools import partial
-
 import jax
+
+jax.config.update("jax_enable_x64", True)
+
 import jax.numpy as jnp
 from jax.sharding import Mesh, PartitionSpec as P
 from jax.experimental import mesh_utils
 from jax.experimental.shard_map import shard_map
+from functools import partial
+
+####
+import os
+os.environ["XLA_FLAGS"] = "--xla_force_host_platform_device_count=" + str(4)
+print(jax.devices("cpu"))
 
 ######################################################
 
-devices = mesh_utils.create_device_mesh((4, 1))
+devices = mesh_utils.create_device_mesh((len(jax.devices()), 1))
 mesh    = Mesh(devices, axis_names=('i', 'j'))
 
 @jax.jit
-def kernel(v_x, v_y):
+def kernel(v_x, v_y, hps=jnp.array([])):
     #return jnp.dot(v_x, v_y)
     return jnp.linalg.norm(v_x - v_y)
 
@@ -23,75 +30,62 @@ def embed_array(small, big):
     start  = tuple( 0          for i in range( small.ndim )       )
     end    = tuple( s + d      for s,d in zip(start, small.shape) )
     slices = tuple( slice(s,e) for s,e in zip(start, end)         )
-
     return big.at[slices].set(small)
 
+#### mesh has to be defined before defining the entire function!!!
 @jax.jit
-def parallel_pair(A, B, kernel=kernel): ## !! alreaady chose A as the larger one
+def parallel_pair(A, B, kernel=kernel):
 
-    #A_split = pmap_dist_prep(A)
-    ### place A & B on the GPU with the right shapes
+    devices = mesh_utils.create_device_mesh((len(jax.devices()), 1))
+    mesh    = Mesh(devices, axis_names=('i', 'j'))
 
-    ###### define pairwise operation
-    @jax.jit
-    def pairwise(R_ix, R_jx, kernel=kernel):
-        """ compute the entire covariance-matrix
-        GET > Σ_ij
-        """
-
-        @jax.jit
-        def matrix_row(v_x, R_ix):
-            """ compute 1 row of the covariance-matrix
-            covariance_vector(v_x, R_ix) = Σ_i
-            """
-            return jax.vmap(kernel, in_axes=(None, 0))(v_x, R_ix)
-
-        @jax.jit
-        def update_matrix(carry, i):
-            """
-            Given the entire covariance-matrix, update the "i"th row
-            with the covariance_vector function
-            """
-            R_ij, R_ix, R_jx = carry
-            R_ij = R_ij.at[i].set(matrix_row(R_ix[i], R_jx))
-            return (R_ij, R_ix, R_jx), None
-
-        N    = R_ix.shape[0]
-        M    = R_jx.shape[0]
-        Σ_ij = jax.numpy.zeros((N, M)) ### define empty covaraince-matrix
-        (Σ_ij, _, __), _ = jax.lax.scan(update_matrix, (Σ_ij, R_ix, R_jx), jax.numpy.arange(N))
-        return Σ_ij
-    ######
+    @partial(jax.vmap, in_axes=(None, 0))
+    def matrix_row(v_x, R_ix):
+        return kernel(v_x, R_ix)
 
     @partial(shard_map, mesh=mesh, in_specs=(P('i'), P(None)), out_specs=P('i'))
     def batched_pairwise(A_batch, B):
-        return pairwise(A_batch, B)
+        return jax.vmap(matrix_row, in_axes=(0, None))(A_batch, B)
 
-    C_split = batched_pairwise(A, B)
+    return batched_pairwise(A, B)
 
-    return C_split
 
-def jjdist(a, b):
+
+
+
+
+
+def jjdist(a, b): ### NOT needed if taken into accout by jdist : Callable[numpy]
     """
     GIVEN > a,b : jnp.ndarray[:,:]
     GET >  c : jnp.ndarray[:,:]
     """
     de    = jnp.ceil( jnp.asarray( a.shape ) / jnp.array( list(mesh.shape.values()) ) ).astype(int)
     large = jax.numpy.zeros( jnp.array( list(mesh.shape.values()) )  * de , dtype=a.dtype )
-    return parallel_pair( embed_array(a, large) , b)[:a.shape[0],:]
+
+    return parallel_pair( embed_array(a, large) , b )[:a.shape[0],:]
 
 import numpy
 
-def jdist(A,B, par=16):
+def jdist(A,B, par=16): ### now we have to determine the partition-size,
+    ### est. max memory per device....
+    ### perhaps a for-loop to try different partitions to see what works.... aim for par=2
     """
     GIVEN > A,B : np.ndarray[N,D] (N samples & D features)
             par : int (the number of seqential partitions)
     GET > distance_matrix : np.ndarray[:,:]
     """
 
+    ### create jax Device-Mesh
+    #devices = mesh_utils.create_device_mesh((4, 1))
+    #mesh    = Mesh(devices, axis_names=('i', 'j'))
+
     C=[]
-    for i in range(par): ## divide A into quarters
+    for i in range(par): ## divide B into pieces (each which divide 4?)
+        ## CPU -> GPU
         a = jnp.asarray( A )
         b = jnp.asarray( B[(B.shape[0]*(i))//par:(B.shape[0]*(i+1))//par,:] )
+        
+        ## carry back from GPU -> CPU
         C.append( numpy.asarray( jjdist(a, b) ) )
     return numpy.concatenate(C, axis=1)
